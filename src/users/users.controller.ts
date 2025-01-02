16c26c96e96f7c774f559ed56d16d866147a2834
hy-reza <hy.rezaalfanda@gmail.com>
2025-01-02 18:26:18 +0700

    Cookie

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,20 +10,24 @@ import {
   Param,
   Post,
   Put,
+  Query,
+  Req,
+  Res,
 } from '@nestjs/common';
+import { Response, Request } from 'express';
 
 @Controller('/api/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get()
+  @Get('/')
   @HttpCode(200)
   @Header('Content-type', 'application/json')
   getUsers() {
     return this.usersService.getUsers();
   }
 
-  @Get('/:id')
+  @Get('/search/:id')
   @HttpCode(200)
   @Header('Content-type', 'application/json')
   getUserById(@Param('id') id: number) {
@@ -49,5 +53,20 @@ export class UsersController {
   @Header('Content-type', 'application/json')
   deleteUser(@Param('id') id: number) {
     return this.usersService.deleteUser(id);
+  }
+
+  @Get('/setCookie')
+  setCookie(@Res() response: Response, @Query('name') name: string): void {
+    response.cookie('name', name);
+    response.status(200);
+    response.send(`Hallo ${name}`);
+  }
+
+  @Get('/getCookie')
+  @HttpCode(200)
+  getCookie(@Req() request: Request): string {
+    return request.cookies['name']
+      ? `${request.cookies['name']}?`
+      : 'Tidak ada kue !';
   }
 }
